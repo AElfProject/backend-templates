4c15c9e085f9bfdc2b974cd98fc42231f99dfb6f
zhifenglee-aelf <zhifeng.lee@aelf.io>
2024-08-05 14:00:11 +0800

    feat(background-worker): added background worker project

---
+++ abp-orleans/src/MyTemplate.Worker/MyTemplateWorkerModule.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+using Volo.Abp.Autofac;
+using Volo.Abp.BackgroundWorkers;
+using Volo.Abp.Modularity;
+
+namespace MyTemplate.Worker;
+
+[DependsOn(
+    typeof(AbpBackgroundWorkersModule),
+    typeof(AbpAutofacModule)
+)]
+public class MyTemplateWorkerModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var configuration = context.Services.GetConfiguration();
+        context.Services.AddHttpClient();
+    }
+
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        context.AddBackgroundWorkerAsync<SyncWorker>();
+    }
+}
---
+++ abp-orleans/src/MyTemplate.Worker/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MyTemplate.Worker.Extensions;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.OpenTelemetry;
+
+namespace MyTemplate.Worker
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            ConfigureLogger();
+            
+            try
+            {
+                Log.Information("Starting Worker.");
+                var builder = WebApplication.CreateBuilder(args);
+                builder.Host
+                    .AddAppSettingsSecretsJson()
+                    .UseOrleansClientConfiguration()
+                    .ConfigureDefaults(args)
+                    .UseAutofac()
+                    .UseSerilog();
+                await builder.AddApplicationAsync<MyTemplateWorkerModule>();
+                var app = builder.Build();
+                await app.InitializeApplicationAsync();
+                await app.RunAsync();
+                await CreateHostBuilder(args).RunConsoleAsync();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly!");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(build => { build.AddJsonFile("appsettings.secrets.json", optional: true); })
+            .ConfigureServices((hostContext, services) => { services.AddApplication<MyTemplateWorkerModule>(); })
+            .UseAutofac()
+            .UseSerilog();
+        
+        private static void ConfigureLogger(LoggerConfiguration? loggerConfiguration = null)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            Log.Logger = (loggerConfiguration ?? new LoggerConfiguration())
+                .ReadFrom.Configuration(configuration)
+#if DEBUG
+                .WriteTo.OpenTelemetry(
+                    endpoint: "http://localhost:4316/v1/logs",
+                    protocol: OtlpProtocol.HttpProtobuf)
+                .MinimumLevel.Debug()
+#else
+                .MinimumLevel.Information()
+#endif
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .CreateLogger();
+        }
+    }
+}
